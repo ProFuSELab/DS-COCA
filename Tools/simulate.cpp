bf72a60efebb71e4eeeb97c198c82bbeb4ea9d59
lalintha <lalintha@ieee.org>
2018-05-09 03:36:50 -0400

    Add simulate.cpp

---
+++ Tools/simulate.cpp
@@ -0,0 +1,49 @@
+#include "DS-Vector/dsvector.h"
+#include "DS-Matrix/dsmatrix.h"
+
+using namespace std;
+
+int main()
+{
+        int index;
+        double element;
+        DSVector ds_vector;
+        DSMatrix ds_matrix;
+        ds_vector.debugOff();
+        ds_vector.readSingletons();
+        index = ds_vector.calFocalIndexAscending();
+        element = ds_vector.accessFocalElement(index);
+        ds_vector.genIncreasingMassValues();
+        ds_vector.fillingBeliefVecAscending();
+        ds_vector.calBelief();
+        ds_vector.fillingBeliefInvVecAscending();
+        ds_vector.calPlausibility();
+        ds_vector.printSingletonVector();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.printSingletonVector();
+        ds_vector.printFocalElements();
+        ds_vector.removeFocalEleSingleton();
+        ds_vector.addFocalEleSingleton();
+        ds_vector.printSingletonVector();
+        ds_vector.printFocalElements();
+            
+        return 0;
+}
