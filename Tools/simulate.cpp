d428592f84bbf997c573eba1a69454119765dc91
lalintha <lalintha@ieee.org>
2018-05-09 03:39:31 -0400

    Update simulate.cpp

--- Tools/simulate.cpp
+++ Tools/simulate.cpp
@@ -1,3 +1,36 @@
+//**************************************************************************************************
+//
+// PROJECT  : Data Structures for Efficient Computation of the Dempster-Shafer Evidential
+//     Calculations and Visualization of Imperfect Data
+// CLASS    : Simulate 
+// PURPOSE  : Simulate
+//
+//**************************************************************************************************
+// 
+// Copyright (c) 2018 ProFuSE Lab - University of Miami 
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+//
+// REVISIONS
+// 
+// Date                   Researcher              Descriptions
+// --------------------   --------------------    --------------------
+// May 2018               Lalintha Polpitiya      Simulate 
+//
+//**************************************************************************************************
+
 #include "DS-Vector/dsvector.h"
 #include "DS-Matrix/dsmatrix.h"
 
