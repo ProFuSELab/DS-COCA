a500d2a6fe3f02e5b19bf88c5ff0e78f82e8b263
lalintha <lalintha@ieee.org>
2018-05-10 03:04:10 -0400

    all_bl_bruteforce_fh_and_ds_10000_2018mbp.out

--- DS-Conditional-One/all-bl-brutefore-fh-and-ds/mbp_and_imac/condmatrix.h
+++ DS-Conditional-One/all-bl-brutefore-fh-and-ds/mbp_and_imac/condmatrix.h
@@ -1,3 +1,36 @@
+//**************************************************************************************************
+//
+// PROJECT  : DS-Conditional-One
+// CLASS    : CondMatrix 
+// PURPOSE  : Simulate
+//
+//**************************************************************************************************
+//
+// Copyright (c) 2018 ProFuSE Lab - University of Miami
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+//
+// REVISIONS
+//
+// Date                   Researcher              Descriptions
+// --------------------   --------------------    --------------------
+// May 2018               Lalintha Polpitiya      DS-Conditional-One 
+//
+//**************************************************************************************************
+
+
 #ifndef _condmatrix_h_
 #define _condmatrix_h_
 
